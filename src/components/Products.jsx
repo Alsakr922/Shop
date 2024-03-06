8dcf0b9209d741ebb1832602bb3255b8b99607c2
Alsakr922 <mo7amedk7aled922@gmail.com>
2024-03-07 01:10:20 +0200

    Final Edit ان شاء الله

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -7,7 +7,7 @@ const Products = () => {
   <div className='pb-[100px] bg-gray-100' id='products'>
     <div className='New container m-auto'>
         <p className='lg:regular-64 regular-40 text-center border-b-2 border-b-second-900 py-10 pb-5 mb-10 mx-[500px] '>All Products</p>
-        <div className="grid grid-cols-3 gap-5">
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5">
         {PRODUCTS_CARD.map((product , index) => (
           <ProductCard type={product.type} key={index} id={product.id} title={product.title} oldPrice={product.oldPrice} newPrice={product.newPrice} img={product.img}/>
         ))}
