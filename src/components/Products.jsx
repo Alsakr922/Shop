6837fd16ff3906b9510d17aba0655186634cd86a
Alsakr922 <mo7amedk7aled922@gmail.com>
2024-03-06 19:41:46 +0200

    Hello 11

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,7 +6,7 @@ const Products = () => {
   return (
   <div className='pb-[100px] bg-gray-100' id='products'>
     <div className='New container m-auto'>
-        <p className='regular-64 text-center border-b-2 border-b-second-900 py-10 pb-5 mb-10 mx-[500px] '>All Products</p>
+        <p className='lg:regular-64 regular-40 text-center border-b-2 border-b-second-900 py-10 pb-5 mb-10 mx-[500px] '>All Products</p>
         <div className="grid grid-cols-3 gap-5">
         {PRODUCTS_CARD.map((product , index) => (
           <ProductCard type={product.type} key={index} id={product.id} title={product.title} oldPrice={product.oldPrice} newPrice={product.newPrice} img={product.img}/>
